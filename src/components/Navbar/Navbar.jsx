2fb391fb4deee68621ad26b015f7dc5b9ae75976
MuhammadBurhan123 <burhantheschoolboy@gmail.com>
2024-06-27 08:10:33 +0500

    Popup in header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -45,7 +45,7 @@ const DropdownLinks = [
   }
 ]
 
-const Navbar = () => {
+const Navbar = ({ handleOrderPopup }) => {
   return (
     <div className='shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
       {/* upper Navbar */}
@@ -65,7 +65,7 @@ const Navbar = () => {
             </div>
           </div>
           {/* order button */}
-          <button onClick={() => alert("Ordering not available yet.")} className='bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3 group'>
+          <button onClick={() => handleOrderPopup()} className='bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3 group'>
             <span className='group-hover:block hidden transition-all duration-200'>Order</span>
             <FaCartShopping className='text-xl text-white drop-shadow-sm cursor-pointer ' />
           </button>
